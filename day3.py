2d9438dc1409d880c0b55c05dae28817d434b12d
Joaquín Monteagudo Gómez <Kindos7@gmail.com>
2023-12-05 22:47:15 +0100

    Day 3 part 1 working

--- day3.py
+++ day3.py
@@ -38,6 +38,30 @@ class NumberRow:
 		))
 
 
+class NumberRowsBuilder:
+	number_rows: list[NumberRow]
+	current_number: NumberRow | None
+
+	def __init__(self):
+		self.number_rows = []
+		self.current_number = None
+
+	def finish_current_number(self) -> None:
+		if self.current_number is not None:
+			self.number_rows.append(self.current_number)
+			self.current_number = None
+
+	def get_numbers(self) -> list[NumberRow]:
+		self.finish_current_number()
+		return self.number_rows
+
+	def add_digit(self, digit: int, coord: Coord) -> None:
+		if self.current_number is None:
+			self.current_number = NumberRow(digit, coord)
+		else:
+			self.current_number.add_digit(digit)
+
+
 @dataclass
 class Day3Input:
 	number_rows: list[NumberRow]
@@ -61,8 +85,7 @@ class Day3(DaySolver):
 		return 0
 
 	def _parse_input(self, path: Path) -> Day3Input:
-		number_rows: list[NumberRow] = []
-		current_number: NumberRow | None = None
+		numbers: list[NumberRow] = []
 		symbol_positions: set[Coord] = set()
 
 		with open(path) as f:
@@ -72,24 +95,21 @@ class Day3(DaySolver):
 			character: str
 
 			for row, line in enumerate(f.readlines()):
-				for column, character in enumerate(line):
+				row_builder: NumberRowsBuilder = NumberRowsBuilder()
+				for column, character in enumerate(line.rstrip("\n")):
 					coord: Coord = Coord(column, row)
 
 					if character.isdigit():
 						digit: int = int(character)
-						if current_number is None:
-							current_number = NumberRow(digit, coord)
-						else:
-							current_number.add_digit(digit)
+						row_builder.add_digit(digit, coord)
 					else:
-						if current_number is not None:
-							number_rows.append(current_number)
-							current_number = None
+						row_builder.finish_current_number()
 
 						if character != ".":
 							symbol_positions.add(coord)
+				numbers.extend(row_builder.get_numbers())
 
-		return Day3Input(number_rows, symbol_positions)
+		return Day3Input(numbers, symbol_positions)
 
 
 if __name__ == '__main__':
